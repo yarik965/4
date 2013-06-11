402643bbaaba2f1a3248f617280d02ac3ed1f77a
Yaroslav_Ovras <ovr_yarik96@ukr.net>
2013-06-11 11:26:28 +0300

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,25 @@
+// 333.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+  int a,b,c,d;
+		cout<<"vvedite 3 storonu trey";
+		cin>>a>>b>>c;
+
+		d=a^2=b^2+c^2-2bc*cos A;
+			if (d=90){
+			cout<<"+";}
+			else {cout<<"-";}
+
+
+
+getch();
+return 0;
+}
+
